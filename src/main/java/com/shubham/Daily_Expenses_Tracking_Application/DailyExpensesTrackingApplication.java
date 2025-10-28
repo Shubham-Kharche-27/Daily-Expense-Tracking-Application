a9572928ff3bcf290ece56329587437b94fed450
Shubham <shubhamkharche2003@gmail.com>
2025-10-28 17:22:43 +0530

    Project basic setup done

---
+++ src/main/java/com/shubham/Daily_Expenses_Tracking_Application/DailyExpensesTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.Daily_Expenses_Tracking_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyExpensesTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyExpensesTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/Daily_Expenses_Tracking_Application/DailyExpensesTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.Daily_Expenses_Tracking_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyExpensesTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
